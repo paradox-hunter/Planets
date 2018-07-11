84a7ba620d7ce7d0de93c19ad63fea8edc1986c6
paradox-hunter <parashar4111@gmail.com>
2018-07-11 20:43:39 +0530

    edited image variable name

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,21 +4,26 @@ public class Planet {
     public double xxVal;
     public double yyVal;
     public double mass;
-    public String ImgFileName;
+    public String imgFileName;
 
     //**constructor function */
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
-        xxPos = xp;
-        yyPos = yp;
-        xxVal = xV;
-        yyVal = yV;
-        mass = m;
-        ImgFileName = img;
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVal = p.xxVal;
+        yyVal = p.yyVal;
+        mass = p.mass;
+        ImgFileName = p.imgFileName;
 
     }
 
     //** constructor for the planet class */
     public Planet(Planet p) {
-        Planet = p;
+        xxPos = xP;
+        yyPos = yP;
+        xxVal = xV;
+        yyVal = yV;
+        mass = m;
+        imgFileName = img;
     }
 }
